2247ce18ac25202f38fbb0c36e286aa3bcb1b378
Jier16 <jq2296@nyu.edu>
2025-06-28 01:25:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -179,10 +179,8 @@ if st.session_state.view_mode == "main":
         f"<p style='font-size:16px;'>Showing articles published between <strong>{DATE_RANGE_START.strftime('%b %d, %Y')}</strong> and <strong>{DATE_RANGE_END.strftime('%b %d, %Y')}</strong>.</p>",
         unsafe_allow_html=True
     )
-
     st.markdown("### 🌐 Website Selection")
     select_all = st.checkbox("Select All Websites")
-
     show_cspi = st.checkbox("Center for Science in the Public Interest", value=select_all)
     show_mighty = st.checkbox("Mighty Earth", value=select_all)
     show_cfs = st.checkbox("Center for Food Safety", value=select_all)
